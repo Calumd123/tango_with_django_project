ae505154d6b469a32c53ce2da5f65cc44f928d4a
Calum Davidson <2317831d@student.gla.ac.uk>
2019-01-27 20:51:14 +0000

    Chapter 5 population script complete

--- rango/models.py
+++ rango/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     def __str__(self): 
         return self.name
     
@@ -13,4 +17,5 @@ class Page(models.Model):
     views = models.IntegerField(default=0)
     
     def __str__(self): 
-        return self.title
+        return self.title
+
